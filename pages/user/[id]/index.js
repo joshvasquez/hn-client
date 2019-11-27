01b823a9befd8306fc30de57cdc969cefcf203f8
Joshua Daisuke Vasquez <daisuke@daisuke.dev>
2019-11-27 01:42:35 -0500

    use the official api for users

--- pages/user/[id]/index.js
+++ pages/user/[id]/index.js
@@ -3,20 +3,23 @@ import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 import Layout from "../../../components/MyLayout";
 import Item from "../../../components/Item";
-const api = `https://api.hackerwebapp.com`;
+// const api = `https://api.hackerwebapp.com`;
+const api = `https://hacker-news.firebaseio.com/v0/`;
 
-const User = props => {
+function User(props) {
+  const about = { __html: props.user.about };
   return (
     <Layout>
       <h1>{props.user.id}</h1>
       <div>karma: {props.user.karma}</div>
-      <div>User created {props.user.created}</div>
+      <div>created: {Date(props.user.created)}</div>
+      <div dangerouslySetInnerHTML={about}></div>
     </Layout>
   );
-};
+}
 
 User.getInitialProps = async function({ res, query }) {
-  const result = await fetch(`${api}/user/${query.id}`);
+  const result = await fetch(`${api}/user/${query.id}.json`);
   const data = await result.json();
   console.log(data);
   return { user: data };
