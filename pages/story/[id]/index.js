bc439a2fc03aa68aafa6ce22460ae482e13de7f8
Joshua Daisuke Vasquez <daisuke@daisuke.dev>
2019-11-27 02:04:45 -0500

    add comments

--- pages/story/[id]/index.js
+++ pages/story/[id]/index.js
@@ -5,10 +5,33 @@ import Layout from "../../../components/MyLayout";
 import Item from "../../../components/Item";
 const api = `https://api.hackerwebapp.com`;
 
+// recursively loops through to nest all comments
+function Comments(props) {
+  const comments = props.comments;
+  return (
+    <ul>
+      <li>
+        {comments.map(function(item) {
+          const content = { __html: item.content };
+          return (
+            <>
+              <h1>{item.user}</h1>
+              <div dangerouslySetInnerHTML={content} />
+              {item.comments ? <Comments comments={item.comments} /> : ""}
+            </>
+          );
+        })}
+      </li>
+    </ul>
+  );
+}
+
 const Story = props => {
   return (
     <Layout>
       <Item item={props.story} />
+
+      <Comments comments={props.story.comments} />
     </Layout>
   );
 };
@@ -16,7 +39,7 @@ const Story = props => {
 Story.getInitialProps = async function({ res, query }) {
   const result = await fetch(`${api}/item/${query.id}`);
   const data = await result.json();
-  console.log(data);
+  console.log(data.comments);
   return { story: data };
 };
 
